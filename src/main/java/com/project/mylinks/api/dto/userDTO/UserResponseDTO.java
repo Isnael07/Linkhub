f2a21ef5b4d4bf5c7bfb93701c688cc460702091
isnael07 <isnaellima24@gmail.com>
2025-10-10 15:36:02 -0300

    chore(dto): adiciona uma lista para links com a tipagem de LinksResponseDTO.

--- src/main/java/com/project/mylinks/api/dto/userDTO/UserResponseDTO.java
+++ src/main/java/com/project/mylinks/api/dto/userDTO/UserResponseDTO.java
@@ -1,9 +1,13 @@
 package com.project.mylinks.api.dto.userDTO;
 
+import com.project.mylinks.api.dto.linksDTO.LinksResponseDTO;
+
+import java.util.List;
 import java.util.UUID;
 
 public record UserResponseDTO(
         UUID id,
         String username,
-        String email
+        String email,
+        List<LinksResponseDTO> links
 ){}
