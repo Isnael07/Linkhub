b91fce41edc3ad1d019912ef27d0b8c27af6247b
isnael07 <isnaellima24@gmail.com>
2025-10-19 21:46:27 -0300

    chore{}: adiciona o novo repository:

--- src/main/java/com/project/mylinks/application/service/UserService.java
+++ src/main/java/com/project/mylinks/application/service/UserService.java
@@ -5,9 +5,8 @@ import com.project.mylinks.api.dto.userDTO.UserResponseDTO;
 import com.project.mylinks.api.dto.userDTO.UserUpdateDTO;
 import com.project.mylinks.application.exception.UserNotFoundExeception;
 import com.project.mylinks.domain.model.User;
-import com.project.mylinks.domain.repository.GenericRepository;
-import com.project.mylinks.infrastructure.persistency.mapper.UserMapper;
-
+import com.project.mylinks.infrastructure.entity.UserEntity;
+import com.project.mylinks.infrastructure.persistency.jpa.UserRepositoryJpa;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,51 +14,55 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.UUID;
 
-import static com.project.mylinks.infrastructure.persistency.mapper.UserMapper.toDomain;
-import static com.project.mylinks.infrastructure.persistency.mapper.UserMapper.toResponse;
+import static com.project.mylinks.infrastructure.persistency.mapper.UserMapper.*;
 
 @Service
 public class UserService {
 
+    private final UserRepositoryJpa repository;
 
-    private final GenericRepository<User, UUID> repository;
-
-    public UserService(GenericRepository<User, UUID> repository) {
+    public UserService(UserRepositoryJpa repository) {
         this.repository = repository;
     }
 
     public UserResponseDTO create(CreateUserDTO dto) {
+
         User domain = toDomain(dto);
-        User saved = this.repository.save(domain);
-        return toResponse(saved);
+
+        UserEntity entity = toEntity(domain);
+
+        UserEntity saved = repository.save(entity);
+        return toResponse(toModel(saved));
     }
 
     public Page<UserResponseDTO> findAll(Pageable pageable) {
-        return this.repository
-                .findAll(pageable)
-                .map(UserMapper::toResponse);
+        return repository.findAll(pageable)
+                .map(entity -> toResponse(toModel(entity)));
     }
 
     public UserResponseDTO findById(UUID id) {
-        return this.repository
-                .findById(id)
-                .map(UserMapper::toResponse)
+        UserEntity entity = repository.findById(id)
                 .orElseThrow(UserNotFoundExeception::new);
+        return toResponse(toModel(entity));
     }
 
     public void delete(UUID id) {
-        this.repository.deleteById(id);
+        if (!repository.existsById(id)) {
+            throw new UserNotFoundExeception();
+        }
+        repository.deleteById(id);
     }
 
+
     public UserResponseDTO update(UUID id, UserUpdateDTO dto) {
-        User exists = this.repository
-                .findById(id)
+        UserEntity entity = repository.findById(id)
                 .orElseThrow(UserNotFoundExeception::new);
 
-        Optional.ofNullable(dto.username()).ifPresent(exists::setUsername);
-        Optional.ofNullable(dto.password()).ifPresent(exists::setPassword);
+        Optional.ofNullable(dto.username()).ifPresent(entity::setUsername);
+        Optional.ofNullable(dto.password()).ifPresent(entity::setPassword);
 
-        User updated = this.repository.save(exists);
-        return toResponse(updated);
+
+        UserEntity updated = repository.save(entity);
+        return toResponse(toModel(updated));
     }
 }
