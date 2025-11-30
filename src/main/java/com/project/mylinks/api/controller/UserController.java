d83d9a70bb33ae6b75851b399d461ef31997b229
isnael07 <isnaellima24@gmail.com>
2025-11-30 16:09:47 -0300

    feat: adiciona preAutorização nos endpoints de user.

--- src/main/java/com/project/mylinks/api/controller/UserController.java
+++ src/main/java/com/project/mylinks/api/controller/UserController.java
@@ -28,23 +28,26 @@ public class UserController {
         return ResponseEntity.ok(this.service.create(dto));
     }
 
+
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<Page<UserResponseDTO>> findALl(Pageable pageable){
         return ResponseEntity.ok(this.service.findAll(pageable));
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> findById(@PathVariable UUID id){
         return ResponseEntity.ok(this.service.findById(id));
     }
 
-    @PreAuthorize("#id.toString() == authentication.name or hasRole('ADMIN')")
+    @PreAuthorize("#id.toString() == authentication.principal or hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable UUID id){
         this.service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("#id.toString() == authentication.name or hasRole('ADMIN')")
     @PatchMapping("/{id}")
     public ResponseEntity<UserResponseDTO> update(@PathVariable UUID id, @RequestBody UserUpdateDTO dto){
         return ResponseEntity.ok(this.service.update(id, dto));
