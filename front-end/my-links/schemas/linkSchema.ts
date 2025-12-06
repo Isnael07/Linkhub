f1ec9447c93dc89591aa5459b07fafe350bf04c8
isnael07 <isnaellima24@gmail.com>
2025-12-05 22:15:38 -0300

    feat: adiciona schema com zod para links

---
+++ front-end/my-links/schemas/linkSchema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const linkSchema = z.object({
+  nameUrl: z
+    .string()
+    .min(3, "O nome deve ter pelo menos 3 caracteres")
+    .max(20, "Máximo de 20 caracteres"),
+
+  url: z
+    .string()
+    .min(1, "A URL é obrigatória"),
+
+  userId: z
+    .uuid("ID do usuário inválido"),
+});
+
+export type LinkFormData = z.infer<typeof linkSchema>;
