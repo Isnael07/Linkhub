d5597ec988ecff1ea0697253f5f4e49fd3b4fe79
isnael07 <isnaellima24@gmail.com>
2025-12-07 20:25:53 -0300

    feat: adiciona configuração de CORS

--- src/main/java/com/project/mylinks/api/controller/AuthController.java
+++ src/main/java/com/project/mylinks/api/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ import java.util.List;
 import java.util.Optional;
 
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class AuthController {
 
