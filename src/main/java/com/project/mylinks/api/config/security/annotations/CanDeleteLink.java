9e03e4ff008b0024c19a7ac11930283654096ddf
isnael07 <isnaellima24@gmail.com>
2025-12-09 19:44:17 -0300

    feat: adiciona anotação personalizada para gerência quem pode remover o link

---
+++ src/main/java/com/project/mylinks/api/config/security/annotations/CanDeleteLink.java
@@ -0,0 +1,15 @@
+package com.project.mylinks.api.config.security.annotations;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("@permissionHandler.canDeleteLink(#id)")
+public @interface CanDeleteLink {
+}
