480eee99bfa17a7e30f79a7daed0ec01cd3e4740
isnael07 <isnaellima24@gmail.com>
2025-12-09 19:34:10 -0300

    chore: muda de diretório

--- src/main/java/com/project/mylinks/api/config/cors/CorsConfig.java
+++ src/main/java/com/project/mylinks/api/config/cors/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.project.mylinks.api.config;
+package com.project.mylinks.api.config.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
