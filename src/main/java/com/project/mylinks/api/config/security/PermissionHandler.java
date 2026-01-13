a811867d70a63f3c32658e7774422c578f453896
isnael07 <isnaellima24@gmail.com>
2026-01-13 14:58:54 -0300

    feat():  adiciona novo método para permissão de criar um link

--- src/main/java/com/project/mylinks/api/config/security/PermissionHandler.java
+++ src/main/java/com/project/mylinks/api/config/security/PermissionHandler.java
@@ -1,5 +1,6 @@
 package com.project.mylinks.api.config.security;
 
+import com.project.mylinks.api.dto.linksDTO.CreateLinksDTO;
 import com.project.mylinks.application.service.LinksService;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +8,8 @@ import java.util.UUID;
 
 @Component("permissionHandler")
 public class PermissionHandler {
+
+    private static final String ADMIM = "ADMIN";
 
     private final LinksService linksService;
 
@@ -22,14 +25,19 @@ public class PermissionHandler {
         if (ownerId == null) return false;
         if (ownerId.equals(userId)) return true;
 
-        return AuthUtil.hasRole("ADMIN");
+        return AuthUtil.hasRole(ADMIM);
+    }
+
+    public boolean canPermissionLinksByUserId(CreateLinksDTO dto){
+        UUID user = AuthUtil.getAuthenticatedUserId();
+        return user.equals(dto.userId()) || AuthUtil.hasRole(ADMIM);
     }
 
     public boolean canPermissionUser(UUID userId){
         UUID id =  AuthUtil.getAuthenticatedUserId();
         if(id.equals(userId)) return true;
 
-        return AuthUtil.hasRole("ADMIN");
+        return AuthUtil.hasRole(ADMIM);
     }
 }
 
