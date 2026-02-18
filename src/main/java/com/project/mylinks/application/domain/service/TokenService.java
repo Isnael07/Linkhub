7265731cc4df6fd7167da83649165062a8895d8c
isnael07 <isnaellima24@gmail.com>
2026-02-18 14:22:24 -0300

    refactor(service):  move métodos generateAccessToken, generateRefreshToken e hash para TokenService

---
+++ src/main/java/com/project/mylinks/application/domain/service/TokenService.java
@@ -0,0 +1,65 @@
+package com.project.mylinks.application.domain.service;
+
+import com.project.mylinks.domain.model.User;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.HexFormat;
+import java.util.List;
+
+@Service
+public class TokenService {
+
+    private final JwtEncoder jwtEncoder;
+
+    public TokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String generateAccessToken(User user){
+        Instant now = Instant.now();
+        long expiresIn = 300L;
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("links-hub-v0")
+                .subject(user.getId().toString())
+                .claim("roles", List.of("ROLE_" + user.getRole().name()))
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .build();
+
+        return jwtEncoder.encode(
+                JwtEncoderParameters.from(
+                        JwsHeader.with(MacAlgorithm.HS256).build(),
+                        claims
+                )
+        ).getTokenValue();
+    }
+
+    public String generateRefreshToken() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[64];
+        random.nextBytes(bytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+    }
+
+    public String hash(String raw) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(raw.getBytes(StandardCharsets.UTF_8));
+            return HexFormat.of().formatHex(hashBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Erro ao gerar hash", e);
+        }
+    }
+}
