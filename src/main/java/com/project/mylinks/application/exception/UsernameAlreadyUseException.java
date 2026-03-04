a1d16ebc773c0577b0e6c2b052a7178372d45770
isnael07 <isnaellima24@gmail.com>
2026-03-04 20:54:45 -0300

    feat(exception): adiciona exception para username já em uso

---
+++ src/main/java/com/project/mylinks/application/exception/UsernameAlreadyUseException.java
@@ -0,0 +1,7 @@
+package com.project.mylinks.application.exception;
+
+public class UsernameAlreadyUseException extends RuntimeException {
+    public UsernameAlreadyUseException() {
+        super("Username já em uso!");
+    }
+}
